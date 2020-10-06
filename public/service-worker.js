15a769daf5f15861ae1d6f9da181f3e6bde963bf
Eric Yarbrough <eric.yarbrough@utexas.edu>
2020-10-06 13:38:32 -0500

    add in manifest

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,4 +1,4 @@
-const APP_PREFIX = 'budget-';
+const APP_PREFIX = 'budget-tracker-';
 const VERSION = 'version_01';
 const CACHE_NAME = APP_PREFIX + VERSION;
 
@@ -7,7 +7,7 @@ const FILES_TO_CACHE = [
     "./js/index.js",
     "./js/ibd.js",
     "./css/styles.css",
-    "./manifest.json",
+    // "./manifest.json",
     "./icons/icon-72x72.png",
     "./icons/icon-96x96.png",
     "./icons/icon-128x128.png",
@@ -17,35 +17,6 @@ const FILES_TO_CACHE = [
     "./icons/icon-384x384.png",
     "./icons/icon-512x512.png"
 ]
-
-self.addEventListener('install', function (e) {
-    e.waitUntil(
-        caches.open(CACHE_NAME).then(function (cache) {
-            console.log('installing cache : ' + CACHE_NAME)
-            return cache.addAll(FILES_TO_CACHE)
-        })
-    )
-});
-
-self.addEventListener('activate', function (e) {
-    e.waitUntil(
-        caches.keys().then(function (keyList) {
-            let cacheKeeplist = keyList.filter(function (key) {
-                return key.indexOf(APP_PREFIX);
-            });
-            cacheKeeplist.push(CACHE_NAME);
-
-            return Promise.all(
-                keyList.map(function (key, i) {
-                    if (cacheKeeplist.indexOf(key) === -1) {
-                        console.log('deleting cache : ' + keyList[i]);
-                        return caches.delete(keyList[i]);
-                    }
-                })
-            );
-        })
-    );
-});
 
 self.addEventListener('fetch', function (e) {
     console.log('fetch request : ' + e.request.url)
@@ -60,4 +31,31 @@ self.addEventListener('fetch', function (e) {
             }
         })
     )
+})
+
+self.addEventListener('install', function (e) {
+    e.waitUntil(
+        caches.open(CACHE_NAME).then(function (cache) {
+            console.log('installing cache : ' + CACHE_NAME)
+            return cache.addAll(FILES_TO_CACHE)
+        })
+    )
+})
+
+self.addEventListener('activate', function (e) {
+    e.waitUntil(
+        caches.keys().then(function (keyList) {
+            let cacheKeeplist = keyList.filter(function (key) {
+                return key.indexOf(APP_PREFIX);
+            })
+            cacheKeeplist.push(CACHE_NAME);
+
+            return Promise.all(keyList.map(function (key, i) {
+                if (cacheKeeplist.indexOf(key) === -1) {
+                    console.log('deleting cache : ' + keyList[i]);
+                    return caches.delete(keyList[i]);
+                }
+            }));
+        })
+    );
 });
